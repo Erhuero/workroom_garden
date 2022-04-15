dbe01d02e08a6d298f7a862821c171c6ce13971d
Constantin <constantin.chtanko@gmail.com>
2022-04-15 18:11:47 +0200

    pop-up windows

--- workhall/src/index.ts
+++ workhall/src/index.ts
@@ -10,13 +10,17 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
     
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+    WA.room.onEnterLayer('ttsZone_1').subscribe(() => {
+        currentPopup = WA.ui.openPopup("tts1Popup","TTS 1 : Automatisation robotisée des processus (RPA)",[]);
     })
 
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+    WA.room.onEnterLayer('ttsZone_2').subscribe(() => {
+        currentPopup = WA.ui.openPopup("tts2Popup","TTS 2 : Des containers dans notre paysage technologique",[]);
+    })
+
+    WA.room.onLeaveLayer('ttsZone_1').subscribe(closePopUp)
+    WA.room.onLeaveLayer('ttsZone_2').subscribe(closePopUp)
+
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,9 +29,41 @@ WA.onInit().then(() => {
     
 }).catch(e => console.error(e));
 
+
 function closePopUp(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
 }
+
+/*
+let messagePopup: any = undefined;
+
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+    
+    WA.room.onEnterLayer('ttsZone2').subscribe(() => {
+        currentPopup = WA.ui.openPopup("messagePopup","TTS 2 : Des containers dans notre paysage technologique",[]);
+    })
+
+    WA.room.onLeaveLayer('ttsZone2').subscribe(closeMessagePopUp)
+
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+    
+}).catch(e => console.error(e));
+
+
+function closeMessagePopUp(){
+    if (messagePopup !== undefined) {
+        messagePopup.close();
+        messagePopup = undefined;
+    }
+}
+*/
