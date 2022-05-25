e319ae117a886c4f29899063d1da33653f2d8daf
Constantin <constantin.chtanko@gmail.com>
2022-05-25 10:42:53 +0200

    vote update

--- workhall/src/index.ts
+++ workhall/src/index.ts
@@ -1,15 +1,16 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { ButtonDescriptor } from "@workadventure/iframe-api-typings/Api/iframe/Ui/ButtonDescriptor";
 
 console.log('Script started successfully');
 
-
+let currentPopup: any;
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
 bootstrapExtra().then(() => {
     console.log('Scripting API Extra ready');
 }).catch(e => console.error(e));
 
-let currentPopup: any = undefined;
+
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -26,7 +27,6 @@ WA.onInit().then(() => {
     WA.state.voteQuatrieme = 0;
 
     WA.room.onEnterLayer('vote1').subscribe(() => {
-        
         (WA.state.votePremier as number)++;
     })
 
